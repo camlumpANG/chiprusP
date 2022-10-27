42f47fdc81c0ca99277e8119a6cf668f8f766716
camlump <camlump@yahoo.com>
2022-10-27 18:34:34 -0400

    created menu for chirpusPizza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
+import { MenuItem } from './models/menu-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  showTitle:boolean = true;
+  pizzaShop = 'Chirpus Pizza';
+item:string[] = ["Cam", "Bill", 'Tom'];
+currentCash:number = 0.42;
+currentDate:Date = new Date();
+
+menuItems:MenuItem[] = [
+  {
+    item:"chicken Fingers",
+    category:"Dinner",
+    price:11.99,
+    special:false
+  },
+  {
+    item:"Pizza",
+    category:"Dinner",
+    price:11.99,
+    special:true
+  },
+  {
+    item:"Wings",
+    category:"Sides",
+    price:8.99,
+    special:false
+  },
+  {
+    item:"Breadsticks",
+    category:"sides",
+    price:8.99,
+    special:false
+  },
+  {
+    item:"Ceasar Salad",
+    category:"Salads",
+    price:5.99,
+    special:false
+  },
+  {
+    item:"Chocolate Lava Cake",
+    category:"Dessert",
+    price:6.99,
+    special:false
+  },
+  {
+    item:"Apple Cobbler",
+    category:"Dessert",
+    price:8.99,
+    special:false
+  },
+]
+
+
+
 }
